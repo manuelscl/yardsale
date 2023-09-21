57d966216649b8f0909cc7d0b9d078da0f997b52
manuelscl <manuelscardenaslopez@gmail.com>
2023-09-21 14:34:17 -0600

    New bikes added

--- main.js
+++ main.js
@@ -2,24 +2,54 @@ const cardsContainer = document.querySelector('.cards-container');
 
 const productList = [];
 productList.push({
-    name: 'Mountain bike Agile',
+    name: 'Raleigh',
     price: 159.56,
     image: './images/mountain_bike_1.jpg'
 });
 productList.push({
-    name: 'Mountain Bike Honor',
+    name: 'Raleigh',
     price: 178.25,
     image: './images/mountain_bike_2.jpg'
 });
 productList.push({
-    name: 'Mountain Elleven Bike Rocker',
+    name: 'Jafi',
     price: 182.10,
     image: './images/mountain_bike_3.jpg'
 });
 productList.push({
-    name: 'Canyon Aro',
+    name: 'Monark',
     price: 410.10,
-    image: './images/mountain_bike_3.jpg'
+    image: './images/mountain_bike_4.jpg'
+});
+productList.push({
+    name: 'Goliat',
+    price: 389.85,
+    image: './images/mountain_bike_5.jpg'
+});
+productList.push({
+    name: 'Jafi Blast',
+    price: 92.99,
+    image: './images/mountain_bike_6.jpg'
+});
+productList.push({
+    name: 'Mountain Bike Pro H',
+    price: 92.99,
+    image: './images/mountain_bike_7.jpg'
+});
+productList.push({
+    name: 'Monark',
+    price: 320.37,
+    image: './images/mountain_bike_8.jpg'
+});
+productList.push({
+    name: 'Monark',
+    price: 320.37,
+    image: './images/mountain_bike_9.jpg'
+});
+productList.push({
+    name: 'Monark',
+    price: 431.30,
+    image: './images/mountain_bike_10.jpg'
 });
 
 function renderProducts(arr) {
